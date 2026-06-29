fe29ba1c1170f3ecaf9290731c3660086b861a79
omjamkhute29 <jamkhuteomjamkhute@gmail.com>
2026-06-30 00:32:35 +0530

    first comment in node

---
+++ one/app.js
@@ -0,0 +1,5 @@
+console.log("Node only run js files only");
+console.log("Node not support window And dom element");
+// its only contain  core functationaly which needed on server side 
+// and ui based element removed
+// like get ele by id
